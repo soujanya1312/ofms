90d660cd6ba408456da0b182ebf832b1449c5762
Dhanush KT <dhanushkt787@gmail.com>
2019-02-05 13:41:07 +0530

    copyright content violation

--- login/accesscontroladmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('connect.php');
-session_start();
-if(!isset($_SESSION['admin']))
-{
-    echo'<script> window.location="403.php"</script>';
-    
-}
-?>
--- login/accesscontrolhead.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-include_once 'connect.php';
-session_start();
-if(!(isset($_SESSION['husername'])))
-{
-	echo'<script> window.location="403.php";</script>';
-}
-?>
