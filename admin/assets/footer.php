ef823f4516dd441eba2b2502af6f1c2709e3785c
Soujanya M <soujanyam1312@gmail.com>
2019-02-16 22:01:59 +0530

    updated ,

--- admin/assets/footer.php
+++ admin/assets/footer.php
@@ -1,2 +1,2 @@
-<footer class="footer text-center"> 2019 &copy; OFMS - Online Fest Management System </footer>
+<footer class="footer text-center">2019 &copy; OFMS - Online Fest Management System</footer>
 
