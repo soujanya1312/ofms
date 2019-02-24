1a5de80e6ffa1da8bb098f3309335f92bc261c56
Soujanya M <soujanyam1312@gmail.com>
2019-02-24 23:26:22 +0530

    register event updated

--- participants/register-event.php
+++ participants/register-event.php
@@ -3,14 +3,15 @@ include '../login/accesscontrolparticipant.php';
 require('connect.php');
 $ausername=$_SESSION['pusername'];
 $id=$_GET['id'];
+
+
+$squery="SELECT eid,ename,edesc,participants FROM events WHERE eid='$id'";
+$sresult = mysqli_query($connection, $squery);
+$row = mysqli_fetch_assoc($sresult);
 ?>
 <!DOCTYPE html>
-<!--
-   This is a starter template page. Use this page to start your new project from
-   scratch. This page gets rid of all links and provides the needed markup only.
-   -->
-<html lang="en">
 
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,131 +37,94 @@ $id=$_GET['id'];
                 <div class="row bg-title">
                     <!-- .page title -->
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Events Participants</h4>
+                    <h4 class="page-title">Events Participants</h4>
                     </div>
-                    <!-- /.page title -->
-                    <!-- .breadcrumb -->
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                       <a href="../index.php" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
-                       <?php echo breadcrumbs(); ?>
+                        <a href="../index.php" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
+                        <?php echo breadcrumbs(); ?>
                     </div>
                     <!-- /.breadcrumb -->
                 </div>
                 <!--DNS added Dashboard content-->
-
-                 <!--DNS Added Model-->
+                <!--DNS Added Model-->
                 <div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                            <h4 class="modal-title">EDIT Instructions</h4>
-                                        </div>
-                                        <div class="modal-body">
-                                       	 To Edit Admin information or to delete Admin account you need to login to that admin account.
-										</div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-                                            <a href="logout.php" class="btn btn-danger waves-effect waves-light">Proceed for login</a>
-                                        </div>
-                                    </div>
-                                </div>
-                    <div class="row p-b-10">
-					<div class="col-md-12 col-sm-10 hvr-wobble-horizontal">
-						<div class="card card-inverse">
-							<img id="theImgId" class="card-img" src="../plugins/images/cards/bg.png" height="120" alt="Card image">
-							<div class="card-img-overlay" style="padding-top: 5px">
-								
-								<p class="card-text" id="cText">Event Name: <!--<?php echo $row['ename']; ?>--></p>
-                                <p class="card-text" id="cText">Event Description:<!-- <?php echo $row['edesc']; ?>--></p>
-                                <p class="card-text" id="cText">No of contestants: <!--<?php echo $row['participants']; ?>--></p>
-							
-								<p class="card-text"><small class="text-white">~OFMS</small></p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-                            </div>
+                     <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                    <h4 class="modal-title">EDIT Instructions</h4>
+                              </div>
+                              <div class="modal-body">
+                                   To Edit Admin information or to delete Admin account you need to login to that admin account.
+				              </div>
+                              <div class="modal-footer">
+                                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                                    <a href="logout.php" class="btn btn-danger waves-effect waves-light">Proceed for login</a>
+                              </div>
+                          </div>
+                    </div>
+             </div>
                          <!--DNS model END-->
     
-                <div class="row p-b-10">
-					<div class="col-md-12 col-sm-10 hvr-wobble-horizontal">
-						<div class="card card-inverse">
+             <div class="row p-b-10">
+				  <div class="col-md-12 col-sm-10 hvr-wobble-horizontal">
+				       <div class="card card-inverse">
 							<img id="theImgId" class="card-img" src="../plugins/images/cards/bg.png" height="120" alt="Card image">
-							<div class="card-img-overlay" style="padding-top: 5px">
-								
-								<p class="card-text" id="cText">Event Name: <!--<?php echo $row['ename']; ?>--></p>
-                                <p class="card-text" id="cText">Event Description:<!-- <?php echo $row['edesc']; ?>--></p>
-                                <p class="card-text" id="cText">No of contestants: <!--<?php echo $row['participants']; ?>--></p>
+				           <div class="card-img-overlay" style="padding-top: 5px">
+								 
+								  <p class="card-text" id="cText">Event Name: <?php echo $row["ename"]; ?></p>
+                                  <p class="card-text" id="cText">Event Description:<?php echo $row["edesc"]; ?></p>
+                                  <p class="card-text" id="cText">No of contestants: <?php echo $row["participants"]; ?></p>
 							
-								<p class="card-text"><small class="text-white">~OFMS</small></p>
-							</div>
-						</div>
-					</div>
+								  <p class="card-text"><small class="text-white">~OFMS</small></p>
+				           </div>
+				     </div>
 				</div>
+             </div>
 
                 <!--row -->
           <div class="row">
-          <div class="col-md-12">
-               <div class="white-box">
-                    <h3 class="box-title m-b-0">Participant Details</h3><hr>
-                    <form data-toggle="validator" method="post">
-                           
-                                <div class="row">
-                                 <!--/span-->
-                                    <div class="col-md-4">
-                                         <div class="form-group">
-                                              <label>Contestant Name:</label>
-                                              <input name="cname" type="text" class="form-control" placeholder="Enter the name" required>
-                                         </div>
-                                    </div>
-                                 <!--/span-->
-                                    <div class="col-md-4">
-                                         <div class="form-group">
-                                              <label>Mobile number:</label>
-                                             <input type="tel" pattern="[0-9]*" maxlength="11" minlength="10" required id="example-phone" name="cmob" class="form-control" placeholder="Enter your mobile number" data-error="Invalid mobile number">
-                     <div class="help-block with-errors"></div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                         <div class="form-group">
-                                               <label>Email ID:</label>
-                                                <input class="form-control" name="cemail" type="email" required placeholder="Email" data-error="This email address is invalid">
-                          <div class="help-block with-errors"></div>
-                                         </div>
-                                     </div>
-                                </div>
+                <div class="col-md-12">
+                      <div class="white-box">
+                            <h3 class="box-title m-b-0">Participant Details</h3><hr>
+                                 <form data-toggle="validator" method="post">
+                                       <div class="row">
+                                          <!--/span-->
+                                             <div class="col-md-4">
+                                                  <div class="form-group">
+                                                    <label>Contestant Name:</label>
+                                                    <input name="cname" type="text" class="form-control" placeholder="Enter the name" required>
+                                                  </div>
+                                              </div>
+                                          <!--/span-->
+                                              <div class="col-md-4">
+                                                   <div class="form-group">
+                                                       <label>Mobile number:</label>
+                                                       <input type="tel" pattern="[0-9]*" maxlength="11" minlength="10" required id="example-phone" name="cmob" class="form-control" placeholder="Enter your mobile number" data-error="Invalid mobile number">
+                                                       <div class="help-block with-errors"></div>
+                                                   </div>
+                                               </div>
+                                              <div class="col-md-4">
+                                                   <div class="form-group">
+                                                        <label>Email ID:</label>
+                                                        <input class="form-control" name="cemail" type="email" required placeholder="Email" data-error="This email address is invalid">
+                                                        <div class="help-block with-errors"></div>
+                                                   </div>
+                                              </div>
+                                           </div>
                             
-                                      <div  class="form-group">
-                                           <center>
-                                           <button type="submit" name="psubmit" class="btn btn-rounded btn-lg btn-info">Submit</button></center>
-                                     </div>
-                    </form>
-                 </div>
+                                          <div  class="form-group">
+                                              <center><button type="submit" name="psubmit" class="btn btn-rounded btn-lg btn-info">Submit</button></center>
+                                         </div>
+                                  </form>
+                       </div>
+                  </div>
             </div>
-         </div>
-
-                <!--/row -->
-
-                <!--DNS End-->
-                <!-- .row -->
-                <!--<div class="row">
-                    <div class="col-md-12">
-                        <div class="white-box">
-                            <h3 class="box-title">Blank Starter page</h3>
-                        </div>
-                    </div>
-                </div>-->
-                <!-- /.row -->
-                <!-- .right-sidebar -->
-                 <!-- Removed Service Panel DNS-->
-                <!-- /.right-sidebar -->
-            </div>
+    </div>
             <!-- /.container-fluid -->
             <!--footer.php contains footer-->
             <?php include'assets/footer.php'; ?>
-        </div>
+    </div>
         <!-- /#page-wrapper -->
     </div>
     <!-- /#wrapper -->
@@ -207,4 +171,3 @@ $(document).ready(function() {
 });
 	
 </script>
-o
