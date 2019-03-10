b5b129988773c0510a9b41855f6d756ae012ce31
Soujanya M <soujanyam1312@gmail.com>
2019-03-10 21:32:10 +0530

    edit-participant corrected

--- participants/edit-participant.php
+++ participants/edit-participant.php
@@ -4,83 +4,66 @@ require('connect.php');
 $ausername=$_SESSION['pusername'];
 $id=$_GET['id'];
 
-$gethname="SELECT pid FROM participants WHERE pusername='$ausername'";
-$gethnameresult=mysqli_query($connection,$gethname);
-$gethnamerow=mysqli_fetch_assoc($gethnameresult);
-$pid=$gethnamerow['pid'];
+$squery="SELECT eid,ename,edesc,hname,addname,hmob,hemail, participants FROM events WHERE eid='$id'";
+$sresult = mysqli_query($connection, $squery);
+$row = mysqli_fetch_assoc($sresult);
+$pari= $row["participants"];
 
+$getpidquery="SELECT pid from participants WHERE pusername='$ausername'";
+$getpidres=mysqli_query($connection, $getpidquery);
+$row2 = mysqli_fetch_assoc($getpidres);
+$pid= $row2["pid"];
 
-$query="SELECT epname,epmob,epemail FROM eventparticipants WHERE pid='$pid' AND eid='$id'";
-$result = mysqli_query($connection, $query);
-$row = mysqli_fetch_assoc($result);
-if(isset($_POST['update']))
+$redirectquery="SELECT * FROM eventparticipants WHERE eid='$id' and pid='$pid'";
+$exeredirect=mysqli_query($connection, $redirectquery);
+$redirect=mysqli_num_rows($exeredirect);
+if($redirect>0)
 {
-	$name= mysqli_real_escape_string($connection,$_POST['name']);
-	$mob= mysqli_real_escape_string($connection,$_POST['mob']);
-	$mail=mysqli_real_escape_string($connection,$_POST['mail']);
-	
-	$uquery="UPDATE eventparticipants SET  epname='$name', epmob='$mob',epemail='$mail' ";
-	$uresult = mysqli_query($connection, $uquery);
-	if($uresult)
-	{
-		$squery="SELECT epname,epmob,epemail FROM eventparticipants";
-		$sresult = mysqli_query($connection, $squery);
-		$row = mysqli_fetch_assoc($sresult);
-		$smsg="participants updated successfully!";
-	}
-	else
-	{
-		$fmsg="participants updation ERROR!";
-	}
-	
-	}
-?>
-<!--html design-->
-<!DOCTYPE html>
-<html lang="en">
+	//$fmsg="WARNING ANYA DO A EDIT REGISTERED EVENT PAGE COZ PARTICIPENTS ARE ALREDY REGISTERED FOR THIS EVENT";
+	//echo'<script> window.location="403.php";</script>';
+}
 
+if (isset($_POST['psubmit']))
+
+	{
+		for($y=1; $y<=$pari; $y++)
+		{
+            $getid=$_POST['cid'.$y.''];
+			$p1=$_POST['cname'.$y.''];
+			$epmob=$_POST['cmob'.$y.''];
+			$epemail=$_POST['cemail'.$y.''];
+			$query="UPDATE eventparticipants SET epname='$p1', epmob='$epmob', epemail='$epemail' WHERE ep_id='$getid'";
+			$result = mysqli_query($connection, $query);
+			if($result)
+			{
+				$smsg = " Participants are registered For the Event".$getid;
+			}
+			else
+			{
+				 $fmsg="Error".mysqli_error($connection);
+			}
+		}
+	}
+	
+
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>OFMS | EDIT</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Online Fest Management System">
     <meta name="author" content="Soujanya M">
     <!--csslink.php includes fevicon and title-->
     <?php include 'assets/csslink.php'; ?>
-      
-    <!-- username check js start -->
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
-	<script type="text/javascript">
-	$(document).ready(function() {
-		$('#e_eventnameLoading').hide();
-		$('#e_eventname').keyup(function(){
-		  $('#e_eventnameLoading').show();
-	      $.post("check-eventname.php", {
-	        username: $('#e_eventname').val()
-	      }, function(response){
-	        $('#e_eventnameResult').fadeOut();
-	        setTimeout("finishAjax('e_eventnameResult', '"+escape(response)+"')", 500);
-	      });
-	    	return false;
-		});
-	});
-
-	function finishAjax(id, response) {
-	  $('#usernameLoading').hide();
-	  $('#'+e_id).html(unescape(response));
-	  $('#'+e_id).fadeIn();
-	} //finishAjax
-</script>
-<!-- username check js end -->
-      
-      
-      
 </head>
 
 <body class="fix-sidebar">
-    <!--header.php includes preloader, top navigarion, logo, user dropdown-->
+     <!--header.php includes preloader, top navigarion, logo, user dropdown-->
     <!--div id wrapper in header.php-->
     <!--left-sidebar.php includes mobile search bar, user profile, menu-->
     <?php include 'assets/header.php';
@@ -88,140 +71,165 @@ if(isset($_POST['update']))
 	include 'assets/breadcrumbs.php';
 	?>
         <!-- Page Content -->
-        <div id="page-wrapper" style="background-image: url(../plugins/images/w.jpg)">
+        <div id="page-wrapper">
             <div class="container-fluid">
                 <div class="row bg-title">
                     <!-- .page title -->
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Edit Participants Details</h4>
+                    <h4 class="page-title">Events Participants</h4>
                     </div>
-                    <!-- /.page title -->
-                    <!-- .breadcrumb -->
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                       <a href="../index.html" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
+                        <a href="../index.php" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
                         <?php echo breadcrumbs(); ?>
                     </div>
                     <!-- /.breadcrumb -->
                 </div>
                 <!--DNS added Dashboard content-->
-                <?php if(isset($fmsg)) { ?>
+                <!--DNS Added Model-->
+                <div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                     <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                    <h4 class="modal-title">EDIT Instructions</h4>
+                              </div>
+                              <div class="modal-body">
+                                   To Edit Admin information or to delete Admin account you need to login to that admin account.
+				              </div>
+                              <div class="modal-footer">
+                                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                                    <a href="logout.php" class="btn btn-danger waves-effect waves-light">Proceed for login</a>
+                              </div>
+                          </div>
+                    </div>
+             </div>
+                         <!--DNS model END-->
+    
+             <div class="row p-b-10">
+				  <div class="col-md-12 col-sm-10 hvr-wobble-horizontal">
+				       <div class="card card-inverse">
+							<img id="theImgId" class="card-img" src="../plugins/images/cards/bg.png" height="200" alt="Card image">
+				           <div class="card-img-overlay" style="padding-top: 5px">
+								
+								
+								  <p class="card-text" id="cText"><u>Event Name:</u> <strong><?php echo $row["ename"]; ?></strong></p>
+                               
+                                  <p class="card-text" id="cText"><u>Event Description:</u> <strong> <?php echo $row["edesc"]; ?></strong></p>
+                               
+                                  <p class="card-text" id="cText"><u>No of contestants:</u> <strong> <?php echo $row["participants"]; ?></strong> </p>
+                               
+                                  <p class="card-text" id="cText"><u>Event Heads:</u> <strong> <?php echo $row["hname"].' '.$row["addname"]; ?></strong> </p>
+                               
+                                  <p class="card-text" id="cText"><u>Mobile Number:</u> <strong><?php echo $row["hmob"]; ?></strong></p>
+                               
+                                  <p class="card-text" id="cText"><u>Email ID:</u> <strong><?php echo $row["hemail"]; ?></strong> </p>
+								  <!--<p class="card-text"><small class="text-white">~OFMS</small></p>-->
+				           </div>
+				     </div>
+				</div>
+             </div>
+              
+                <!--row -->
+          <div class="row">
+                <div class="col-md-12">
+                      <div class="white-box">
+                            <h3 class="box-title m-b-0">Enter Participant Details</h3><hr>
+                                 <form data-toggle="validator" method="post">
+                                     <?php if(isset($fmsg)) { ?>
 									<div class="alert alert-danger alert-dismissable">
 										<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 										 <?php echo $fmsg; ?>
-									</div> 
-					            <?php }?> 
-							<?php if(isset($smsg)) { ?>
-									<div class="alert alert-success alert-dismissable">
-										<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-										 <?php echo $smsg; ?>
-									</div> 
-				<?php }?>
-                <div class="row">
-                    <div class="col-md-4 col-xs-12">
-                        <div class="white-box">
-                           <div class="overlay-box">
-							<div class="user-bg p-l-30 m-l-10 p-t-10"> <img width="80%" height="95%"  alt="user" src="../plugins/images/index1.jpg">
-                                  <div class="user-content">
-                                      <h3>
-                                       <p><strong><?php echo $gethnamerow["ename"]; ?></strong>
-                                       <p><strong><?php echo $gethnamerow["edesc"]; ?></strong>
-                                      </h3>
-                                       </div>
-                                </div>
-                            </div>
-                         </div>
-                    </div>
-                    <div class="col-md-8 col-xs-12">
-                        <div class="white-box">
-                            <ul class="nav customtab nav-tabs" role="tablist">
-                                <!--<li role="presentation" class="nav-item"><a href="#home" class="nav-link " aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true"><span class="visible-xs"><i class="fa fa-home"></i></span><span class="hidden-xs"> Activity</span></a></li>-->
-                                <li role="presentation" class="nav-item"><a href="#profile" class="nav-link active" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="true"><span class="visible-xs"><i class="fa fa-user"></i></span> <span class="hidden-xs">Profile</span></a></li>
-                                <li role="presentation" class="nav-item">
-								<a href="#settings" class="nav-link" aria-controls="settings" role="tab" data-toggle="tab" aria-expanded="false"><span class="visible-xs"><i class="fa fa-cog"></i></span> <span class="hidden-xs">Setting</span></a></li>
-                                <li role="presentation" class="nav-item">
-                            </ul>
-                           
-							<div class="tab-content">
-                                <?php foreach($result as $key=>$result) {  ?>
-                                <div class="tab-pane active" id="profile">
-                                    <div class="row">
-                                            <div class="col-md-3 col-xs-6 b-r"> Participants:
-                                            <br><br>
-                                            <p><strong><?php echo $result["epname"]; ?></strong></p>
-                                        </div>
-                                      
-                                        <div class="col-md-6 col-xs-6 "> Email :
-                                            <br>
-                                            <p><strong><?php echo $result['epemail']?></strong></p>
-                                      	 Mob :
-                                            <p><strong><?php echo $result['epmob']?></strong></p>
+									</div>
+					            <?php }?>
+								<?php if(isset($smsg)) { ?>
+										<div class="alert alert-success alert-dismissable">
+											<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+											 <?php echo $smsg; ?>
 										</div>
-                                        
-                                    </div>
-                                     <div class="row">
-                                         <div class="col-md-3 col-xs-6 b-r">       
-                                              <p class="text-muted"></p>
+								<?php }?>
+									 <?php foreach($exeredirect as $key=>$exeredirect)  {  ?>
+                                       <div class="row">
+                                          <!--/span-->
+                                             <div class="col-md-4">
+                                                  <div class="form-group">
+                                                    <label>Contestant <?php echo $key+1; ?> Name:</label>
+                                                    <input name="cname<?php echo $key+1; ?>" type="text" class="form-control" value="<?php echo $exeredirect["epname"] ?>" placeholder="Enter the name" required>
+                                                  </div>
+                                              </div>
+                                          <!--/span-->
+                                              <div class="col-md-4">
+                                                   <div class="form-group">
+                                                       <label>Mobile number:</label>
+                                                       <input type="tel" pattern="[0-9]*" maxlength="11" minlength="10" id="example-phone" value="<?php echo $exeredirect["epmob"] ?>" name="cmob<?php echo $key+1; ?>" class="form-control" placeholder="Enter your mobile number" data-error="Invalid mobile number">
+                                                       <div class="help-block with-errors"></div>
+                                                   </div>
+                                               </div>
+                                              <div class="col-md-4">
+                                                   <div class="form-group">
+                                                        <label>Email ID:</label>
+                                                        <input class="form-control" name="cemail<?php echo $key+1; ?>" type="email" value="<?php echo $exeredirect["epemail"] ?>" placeholder="Email" data-error="This email address is invalid">
+                                                        <div class="help-block with-errors"></div>
+                                                   </div>
+                                              </div>
+                                           </div>
+                                     <input name="cid<?php echo $key+1; ?>" type="hidden" class="form-control" value="<?php echo $exeredirect["ep_id"]; ?>" >
+                            <?php } ?>
+                                          <div  class="form-group">
+                                              <center><button class="btn btn-rounded btn-lg btn-info">Edit</button>
+                                                  <button type="submit" name="psubmit" class="btn btn-rounded btn-lg btn-info">Submit</button></center>
                                          </div>
-                                          
-                                         <div class="col-md-3 col-xs-6 b-r">                              <p class="text-muted"></p>
-                                         </div> 
-                                         <div class="col-md-3 col-xs-6">                                   <p class="text-muted"></p>
-                                          </div>
-								    </div>
-                        </div>
-                                <?php } ?>
-                                
-                               
-                            <div class="tab-pane" id="settings">
-                             <form data-toggle="validator" method="post">
-                              
-                              
-                         		<div class="row">
-                                	<div class="col-md-6">
-                                       <div class="form-group">
-                                        	   <div class="row">
-                           				   <div class="form-group col-md-12">
-											 <label class="control-label ">Participants</label>
-											<input type="text" id="username" name="name" class="form-control" value="<?php echo $row['epname']; ?>" readonly/>  
-								  			</div>   
-                                              <div class="form-group col-md-12">
-											 <label class="control-label ">Email</label>
-											<input type="text" id="username" name="mail" class="form-control" value="<?php echo $row['epemail']; ?>" readonly/>  
-								  			</div>         
-                                	       <div class="form-group col-md-12">
-											 <label class="control-label ">Mobile Number</label>
-											<input type="text" id="username" name="mob" class="form-control" value="<?php echo $row['epmob']; ?>" readonly/>  
-								  			</div>  
-							</div>
-								 </div>
-										 <div class="form-group p-t-0">
-                                       <button class="btn btn-success" name="update">Update Contestants </button>
-										</div>
-								 </div>
-								</div>
-                                </div>
-                                
-								</form>
-                                
-                                
-								</div>
-                               
-                            </div>
-                        </div>
-                    </div>
-                </div>
-							
-							
-							
+                                  </form>
+                       </div>
+                  </div>
+            </div>
+    </div>
             <!-- /.container-fluid -->
             <!--footer.php contains footer-->
             <?php include'assets/footer.php'; ?>
-        </div>
+    </div>
         <!-- /#page-wrapper -->
     </div>
     <!-- /#wrapper -->
     <!--jslink has all the JQuery links-->
     <?php include'assets/jslink.php'; ?>
+    
+    <!--    function(){
+            swal("Deleted!", "User has been deleted.", "success");
+            window.location.replace("view-doctors.php");     -->
 </body>
 
 </html>
+<script>
+$(document).ready(function() {
+  $('.deleteStaff').click(function(){
+    id = $(this).attr('data-id');
+      swal({
+          title: "Are you sure?",
+          text: "You will not be able to recover this data!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Yes, delete it!",
+          closeOnConfirm: false,
+		  closeOnCancel: false
+      },function(isConfirm)
+		 {   
+           if (isConfirm) {
+			   $.ajax({
+			  url: 'deletestaff.php?id='+id,
+			  type: 'DELETE',
+			  data: {id:id},
+			  success: function(){
+				swal("Deleted!", "User has been deleted.", "success");
+				window.location.replace("view-staffs.php");
+          }
+        });   
+            } else {     
+                swal("Cancelled", "User data is safe :)", "error");   
+            }
+      });
+  });
+
+});
+	
+</script>
