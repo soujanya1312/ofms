e06d599845df213e232d813ffdbecc0354642185
Soujanya M <soujanyam1312@gmail.com>
2019-03-17 18:49:57 +0530

    view-details corrected

--- admin/view-details.php
+++ admin/view-details.php
@@ -1,143 +1,222 @@
 <?php
-include 'file:///C|/xampp/htdocs/login/accesscontroladmin.php';
-require('../connect.php');
+include '../login/accesscontroladmin.php';
+require('connect.php');
 $ausername=$_SESSION['admin'];
-$id=$_GET['id'];
+
 //$squery="SELECT *,eventparticipants.epname,eventparticipants.epmob,eventparticipants.epemail FROM eventparticipants INNER JOIN events ON eventparticipants.eid = eventseid WHERE ep_id='$id'";
-$squery="SELECT eid,ename,edesc,hname,addname,hmob,hemail, participants FROM events WHERE eid='$id'";
+$squery="SELECT eid,ename,edesc,hname,addname,hmob,hemail, participants FROM events";
 $sresult = mysqli_query($connection, $squery);
 $row = mysqli_fetch_assoc($sresult);
 $pari= $row["participants"];
 ?>
-<!DOCTYPE html>
 
+  <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Online Fest Management System">
-    <meta name="author" content="Soujanya M">
-    <!--csslink.php includes fevicon and title-->
-    <?php include '../assets/csslink.php'; ?>
-</head>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
+    <title>OFMS-EventRegister</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
+    <!-- animation CSS -->
+    <link href="../plugins/css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../plugins/css/style.css" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="../plugins/css/colors/blue.css" id="theme" rel="stylesheet">
+    <!--alerts CSS -->
+    <link href="../plugins/bower_components/sweetalert/sweetalert.css" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="../plugins/bower_components/bootstrap-datepicker/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css" />
+	<link href="../plugins/bower_components/bootstrap-select/bootstrap-select.min.css" rel="stylesheet" />
+    	<script>
+		function isFutureDate(idate){
+    var today = new Date().getTime(),
+        idate = idate.split("-");
 
-<body class="fix-sidebar">
-     <!--header.php includes preloader, top navigarion, logo, user dropdown-->
-    <!--div id wrapper in header.php-->
-    <!--left-sidebar.php includes mobile search bar, user profile, menu-->
-    <?php include '../assets/header.php';
-	include '../assets/left-sidebar.php';
-	include '../assets/breadcrumbs.php';
-	?>
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row bg-title">
-                    <!-- .page title -->
-                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                    <h4 class="page-title">Events Participants</h4>
-                    </div>
-                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                        <a href="file:///C|/xampp/htdocs/index.php" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
-                        <?php echo breadcrumbs(); ?>
-                    </div>
-                    <!-- /.breadcrumb -->
-                </div>
-                <!--DNS added Dashboard content-->
-                <!--DNS Added Model-->
-                <div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-                     <div class="modal-dialog">
-                          <div class="modal-content">
-                              <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                    <h4 class="modal-title">EDIT Instructions</h4>
-                              </div>
-                              <div class="modal-body">
-                                   To Edit Admin information or to delete Admin account you need to login to that admin account.
-				              </div>
-                              <div class="modal-footer">
-                                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-                                    <a href="../logout.php" class="btn btn-danger waves-effect waves-light">Proceed for login</a>
-                              </div>
-                          </div>
-                    </div>
-             </div>
-                         <!--DNS model END-->
-    
- <div class="row p-b-10">
-				  <div class="col-md-12 col-sm-10 hvr-wobble-horizontal">
-				       <div class="card card-inverse">
-							<img id="theImgId" class="card-img" src="file:///C|/xampp/htdocs/plugins/images/cards/bg.png" height="200" alt="Card image">
-				           <div class="card-img-overlay" style="padding-top: 5px">
-								
-								  <p class="card-text" id="cText"><u>Event Name:</u> <strong><?php echo $row["ename"]; ?></strong></p>
-                               
-                                  <p class="card-text" id="cText"><u>Event Description:</u> <strong> <?php echo $row["edesc"]; ?></strong></p>
-                               
-                                  <p class="card-text" id="cText"><u>No of contestants:</u> <strong> <?php echo $row["participants"]; ?></strong> </p>
-                               
-                                  <p class="card-text" id="cText"><u>Event Heads:</u> <strong> <?php echo $row["hname"].' '.$row["addname"]; ?></strong> </p>
-                               
-                                  <p class="card-text" id="cText"><u>Mobile Number:</u> <strong><?php echo $row["hmob"]; ?></strong></p>
-                               
-                                  <p class="card-text" id="cText"><u>Email ID:</u> <strong><?php echo $row["hemail"]; ?></strong> </p>
-							
-								  <!--<p class="card-text"><small class="text-white">~OFMS</small></p>-->
-				           </div>
-				     </div>
-				</div>
-             </div>
-           </div>
-            <!-- /.container-fluid -->
-            <!--footer.php contains footer-->
-            <?php include'../assets/footer.php'; ?>
-    </div>
-        <!-- /#page-wrapper -->
-    </div>
-    <!-- /#wrapper -->
-    <!--jslink has all the JQuery links-->
-    <?php include'../assets/jslink.php'; ?>
-    
-    <!--    function(){
-            swal("Deleted!", "User has been deleted.", "success");
-            window.location.replace("view-doctors.php");     -->
-</body>
-
-</html>
-<script>
-$(document).ready(function() {
-  $('.deleteStaff').click(function(){
-    id = $(this).attr('data-id');
-      swal({
-          title: "Are you sure?",
-          text: "You will not be able to recover this data!",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#DD6B55",
-          confirmButtonText: "Yes, delete it!",
-          closeOnConfirm: false,
-		  closeOnCancel: false
-      },function(isConfirm)
-		 {   
-           if (isConfirm) {
-			   $.ajax({
-			  url: 'deletestaff.php?id='+id,
-			  type: 'DELETE',
-			  data: {id:id},
-			  success: function(){
-				swal("Deleted!", "User has been deleted.", "success");
-				window.location.replace("view-staffs.php");
-          }
-        });   
-            } else {     
-                swal("Cancelled", "User data is safe :)", "error");   
-            }
-      });
-  });
-
-});
+    idate = new Date(idate[2], idate[1] - 1, idate[0]).getTime();
+    return (today - idate) > 0 ? true : false;
+    }
+	</script>
 	
-</script>
-     
+</head>
+<body>
+    <!-- Preloader -->
+<div class="preloader">
+    <div class="cssload-speeding-wheel"></div>
+</div>
+<section id="wrapper" class="login-register" style="overflow: scroll">
+    <?php if(isset($fmsg)) { ?>
+            <div class="alert alert-danger alert-dismissable">
+                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                 <?php echo $fmsg; ?>
+            </div> 
+            <?php }?> 
+            <?php if(isset($smsg)) { ?>
+             <div class="alert alert-success alert-dismissable">
+				  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+				  <?php echo $smsg; 
+                  echo '<script> window.setTimeout(function(){
+				  swal("Event registration is Completed!", "Redirecting to login in 4 seconds.","success");
+				  }, 300);  window.setTimeout(function(){
+				  window.location.href = "../login/logout.php";
+				  }, 4000); </script>'
+				  ?>
+              </div> 
+              <?php }?>
+<div style="padding-top: 90px; padding-left: 60px; padding-right: 60px; padding-bottom: 40px">
+     <div class="row">
+          <div class="col-sm-12">
+               <div class="white-box">
+                <form data-toggle="validator" method="post">
+               <h3 class="box-title m-b-0">Fest Details</h3><hr>
+                                    <?php
+                                      $getpidquery="SELECT fid,fname,fdate,ftype,fdesc from fests ";
+					                  $getpidres=mysqli_query($connection, $getpidquery);
+					                  $row2 = mysqli_fetch_assoc($getpidres);
+                                                ?>
+                    
+                                <div class="row">
+                                	 <div class="col-md-6" >
+                                          <div class="form-group">
+                                               <label class="control-label">Fest Name</label>
+								                       <div class="col-sm-12 p-l-0">
+								                            <div class="input-group">
+								                                 <input type="text" name="dname" class="form-control" placeholder="Fest Name" disabled value="<?php echo $row2["fname"];?>">
+								                            </div>
+				                                       </div>
+                                         </div>
+                                    </div>
+                                     
+                                       <div class="col-md-6" >
+                                            <div class="form-group">                                   
+                                                 <label class="control-label">Fest Date</label>
+                                                      <div class="input-group">
+								                         <input onChange="checkDate();" onKeyUp="checkDate();" data-date-format="dd-mm-yyyy" type="text" class="form-control" data-mask="99-99-9999" id="datepicker" name="fdate" placeholder="dd-mm-yyyy" required   disabled value="<?php echo $row2["fdate"];?>">
+								               </div>
+								                     <div id="datewarn"></div>
+                                                              <!--<span class="font-13 text-muted">dd-mm-yyyy</span>-->
+                                             </div>
+                                        </div>
+                                   </div>
+
+                                   <div class="form-group">
+                                    <label class="col-sm-12 p-l-0">Fest Type</label>
+                                    <div class="col-sm-12 p-l-0">
+                                        <select class="form-control" name="ftype"  disabled value="<?php echo $row2["ftype"];?>">
+                                            <option selected hidden disabled>Select Your FestType</option>
+                                            <option value="IT">IT</option>
+                                            <option value="Management">MANAGEMENT</option>
+                                            <option value="Cultural">CULTURAL</option>
+                                            <option value="Others">OTHERS</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <label class="control-label">Fest Description</label>
+                                <div class="col-md-12 p-l-0">
+                                     <div class="form-group">
+                                          <textarea input type="text" name="fdesc" class="form-control" disabled placeholder="Tell us about your fest">  <?php echo $row2["fdesc"];?></textarea>
+                                     </div>
+                                </div>
+
+                    <h3 class="box-title m-b-0">Event Details</h3><hr>
+                    
+                       <div class="row">
+                <?php
+                   $getidquery="SELECT fests.fid FROM admin JOIN fests ON admin.aid=fests.aid WHERE ausername='$ausername'";
+                   $getidresult = mysqli_query($connection, $getidquery);
+                  $getidrow = mysqli_fetch_assoc($getidresult);
+                    $fid=$getidrow['fid'];
+
+
+					$query = "SELECT eid,ename,edesc,erounds,participants,hemail,hmob FROM events WHERE fid='$fid'";
+					$result = mysqli_query($connection, $query);
+				foreach($result as $key=>$result)
+				{ ?>
+                <div class="col-md-4 col-sm-4">
+                        <div class="white-box">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4 text-center">
+                                        <img src="../plugins/images/index4.jpg" class="img-square img-responsive"> 
+                                </div>
+                                <div class="col-md-8 col-sm-8">
+                                    <h2 class="box-title m-b-0"><?php echo $result["ename"]; ?></h2>
+                                   <strong> Event description:</strong><?php echo $result["edesc"]; ?><br>
+                                    <strong> Event Rounds:</strong> <?php echo $result["erounds"]; ?><br>
+                                    <strong> Event participants:</strong> <?php echo $result["participants"]; ?>
+                                    
+                                    <p calss="p-0">
+										<a href="mailto:<?php echo $result["hemail"]; ?>"> <font size="-1"> <?php echo $result["hemail"]; ?> </font> </a> <br>
+                                        <a href="mailto:<?php echo $result["hemail"]; ?>"> <font size="-1"> <?php echo $result["hmob"]; ?> </font> </a>
+										
+										
+                                    </p>
+									
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  <?php
+					}
+				  ?>
+
+				</div>
+                            
+                               
+                               
+                    </form>
+                 </div>
+            </div>
+         </div>
+    </div>
+</section>
+    <!-- jQuery -->
+    <script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../plugins/bootstrap/dist/js/tether.min.js"></script>
+    <script src="../plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="../plugins/js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="../plugins/js/waves.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="../plugins/js/custom.min.js"></script>
+    <!--Style Switcher -->
+    <script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+    
+    <script src="../plugins/js/validator.js"></script>
+    <!-- Sweet-Alert  -->
+    <script src="../plugins/bower_components/sweetalert/sweetalert.min.js"></script>
+    <script src="../plugins/bower_components/sweetalert/jquery.sweet-alert.custom.js"></script>
+    <!-- Date Picker Plugin JavaScript -->
+    <script src="../plugins/bower_components/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="../plugins/js/mask.js"></script>
+    <script>
+		jQuery('.mydatepicker, #datepicker').datepicker();
+    jQuery('#datepicker-autoclose').datepicker({
+        autoclose: true,
+        todayHighlight: true
+    });
+	</script>
+	<script>
+		jQuery('.mydatepicker, #datepicker').datepicker();
+    jQuery('#datepicker-autoclose1').datepicker({
+        autoclose: true,
+        todayHighlight: true
+    });
+	</script>
+</body>
+</html>
