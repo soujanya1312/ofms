a86884ca3bc14373e1510fff7e575e7e01769a6b
Dhanush KT <dhanushkt787@gmail.com>
2019-03-21 18:55:44 +0530

    added fest-settings page

---
+++ admin/fest-settings.php
@@ -0,0 +1,113 @@
+<?php
+include '../login/accesscontroladmin.php';
+require('connect.php');
+$ausername=$_SESSION['admin'];
+?>
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Online Fest Management System">
+    <meta name="author" content="Soujanya M">
+    <!--csslink.php includes fevicon and title-->
+    <?php include 'assets/csslink.php'; ?>
+<!-- username check js start--->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#usernameLoading').hide();
+		$('#username').keyup(function(){
+		  $('#usernameLoading').show();
+	      $.post("check-eventusername.php", {
+	        username: $('#username').val()
+	      }, function(response){
+	        $('#usernameResult').fadeOut();
+	        setTimeout("finishAjax('usernameResult', '"+escape(response)+"')", 500);
+	      });
+	    	return false;
+		});
+	});
+
+	function finishAjax(id, response) {
+	  $('#usernameLoading').hide();
+	  $('#'+id).html(unescape(response));
+	  $('#'+id).fadeIn();
+	} //finishAjax
+</script>
+<!-- username check js end -->
+</head>
+
+<body class="fix-sidebar">
+    <!--header.php includes preloader, top navigarion, logo, user dropdown-->
+    <!--div id wrapper in header.php-->
+    <!--left-sidebar.php includes mobile search bar, user profile, menu-->
+    <?php include 'assets/header.php';
+		include 'assets/left-sidebar.php';
+		include 'assets/breadcrumbs.php';
+	?>
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Fest settings</h4>
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <a href="../index.php" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
+                        <?php echo breadcrumbs(); ?>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+
+				<!--- imported add-doctors---->
+				<!--Script to copy the input from fname to username-->
+
+				<div class="row">
+				<div class="col-sm-12">
+                        <div class="white-box">
+                            <h3 class="box-title m-b-0">Fest settings</h3>
+							<?php if(isset($fmsg)) { ?>
+									<div class="alert alert-danger alert-dismissable">
+										<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+										 <?php echo $fmsg; ?>
+									</div>
+					            <?php }?>
+							<?php if(isset($smsg)) { ?>
+									<div class="alert alert-success alert-dismissable">
+										<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+										 <?php echo $smsg; ?>
+									</div>
+							<?php }?>
+							<div>
+								Fest is not public and is not open for regidstrstion
+							</div>
+                        
+                        </div>
+                    </div>
+				</div>
+				<!---End of impoted--->
+                <!-- .right-sidebar -->
+                <!--DNS Removed Service Panel-->
+                <!-- /.right-sidebar -->
+            </div>
+            <!-- /.container-fluid -->
+            <!--footer.php contains footer-->
+            <?php include'assets/footer.php'; ?>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!--jslink has all the JQuery links-->
+    <?php include'assets/jslink.php'; ?>
+</body>
+
+</html>
